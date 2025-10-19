d70b49ef4daefb99ade93e4d193ac90f4a279716
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-10-19 14:27:38 +0500

    added login and registration pages that is working and data is being stored

--- devconnect/client/src/pages/Login.jsx
+++ devconnect/client/src/pages/Login.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import { useState } from 'react';
+import api from '../api';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+   const navigate = useNavigate();
+   const [form, setForm] = useState({email: "", password: ""});
+
+   const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value});
+   }
+
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.post("/auth/login", form);
+      localStorage.setItem("token", res.data.token);
+      alert("Login successful!");
+      navigate("/dashboard");
+
+
+    } catch (err) {
+      alert(err.response?.data?.message || "Login failed");
+
+    }
+   }
+
   return (
-    <div className='text-2xl font-bold'>Login Page</div>
+    <div className='max-w-md mx-auto'>
+      <h1 className='text-3xl font-bold mb-4'>Login</h1>
+      <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
+            <input
+          className="border p-2"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          required
+        />
+        <input
+          className="border p-2"
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+          required
+        />
+
+        <button className='bg-green-500 text-white p-2'>Login</button>
+      </form>
+    </div>
   )
 }
 
--- devconnect/client/src/pages/Register.jsx
+++ devconnect/client/src/pages/Register.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import api from '../api';
+import { useNavigate } from 'react-router-dom'
+import { useState }  from 'react';
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({ name: "", email: "", password: ""});
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value});
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        await api.post("/auth/register", form);
+        navigate("/login");
+    } catch (err){
+      alert(err.response?.data?.message || "Registration failed");
+
+    }
+  };
+
   return (
-    <div className='text-2xl font-bold'>Register Page</div>
+    <div className='max-w-md mx-auto'>
+      <h1 className='text-3xl font-bold mb-4'>Register</h1>
+      <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
+
+        <input className='border p-2' name="name" placeholder='Name' onChange= {handleChange} required   />
+
+         <input
+          className="border p-2"
+          name="email"
+          type="email"
+          placeholder="Email"
+          onChange={handleChange}
+          required
+        />
+
+           <input
+          className="border p-2"
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+          required
+        />
+        <button className='bg-blue-500 text-white p-2 mt-2'>Register</button>
+
+      </form>
+
+      
+    </div>
   )
 }
 
